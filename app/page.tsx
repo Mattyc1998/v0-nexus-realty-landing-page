759b3feea503aba4c5b1473b2615fa97cf5b62b5
Mattyc1998 <matthew.crawford23@aol.com>
2026-02-10 21:20:40 +0000

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { FeaturedProperties } from "@/components/sections/featured-properties"
 import { Advantages } from "@/components/sections/advantages"
 import { NeighborhoodExplorer } from "@/components/sections/neighborhood-explorer"
 import { RecentlySold } from "@/components/sections/recently-sold"
-import { FAQ } from "@/components/sections/faq"
 import { Testimonials } from "@/components/sections/testimonials"
+import { FAQ } from "@/components/sections/faq"
 import { CTA } from "@/components/sections/cta"
 import { Footer } from "@/components/sections/footer"
 import { BackToTop } from "@/components/back-to-top"
@@ -22,8 +22,8 @@ export default function Page() {
         <Advantages />
         <NeighborhoodExplorer />
         <RecentlySold />
-        <FAQ />
         <Testimonials />
+        <FAQ />
         <CTA />
       </main>
       <Footer />
